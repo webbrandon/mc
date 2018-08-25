78649fabb34779bf824f70e7fea2a52f0129cbf3
Brandon Clark <brandon.clark@leafgroup.com>
2018-08-25 10:13:11 -0700

    Apply new option to logging.
    This is very simplistic logging.  More complex logging should be
    considered down the line.

--- src/main.rs
+++ src/main.rs
@@ -12,29 +12,35 @@ mod file;
 use model::{Scripts, Template};
 use handler::{Configs};
 
+fn log_it(mute: bool, content: String) {
+    if ! mute {
+        file::out_term(&content);
+    }
+}
+
 fn main() {
     let matches = cli::build_cli().get_matches();
     let request = &mut Configs::process_args(&matches);
     let mut scripts = Scripts::new();
     let mut templates = Template::new();
-    let mute = &matches.is_present("mute");
+    let mute = matches.is_present("mute");
     
     templates.load_templates(request);
     scripts.load_scripts(request);
     
     if request.has_script() {
-        file::out_term(&scripts.process_script());
+        log_it(mute, scripts.process_script());
     }
     
     if request.has_template() && request.has_params() {
         templates.render_template();
         if request.has_render() {
             file::outfile(request.render(), templates.render());
-            file::out_term(&templates.render());
+            log_it(mute, templates.render().to_string());
         }
     }
     
     if request.has_post_script() {
-        file::out_term(&scripts.process_post_script());
+        log_it(mute, scripts.process_post_script());
     }
 }
