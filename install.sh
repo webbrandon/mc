01c8bbd2408a8fadcc034048af7fc1b32aba84ac
Brandon Clark <brandon.clark@leafgroup.com>
2018-12-14 21:02:33 -0800

    make executable

--- install.sh
+++ install.sh
@@ -5,11 +5,13 @@ if [[ "$OSTYPE" == "linux-gnu" ]]; then
   echo "Downloading debian client."
   curl -LO https://github.com/webbrandon/mc/releases/download/${VERSION}/debian
   echo "Installing Master of Ceremony ${VERSION}"
+  chmod +x debian
   mv debian /usr/local/bin/mc
 elif [[ "$OSTYPE" == "darwin"* ]]; then
   echo "Downloading darwin client."
   curl -LO https://github.com/webbrandon/mc/releases/download/${VERSION}/darwin
   echo "Installing Master of Ceremony ${VERSION}"
+  chmod +x darwin
   mv darwin /usr/local/bin/mc
 else
   printf "System not supported. Attempting to build from source. You must have rust installed."
@@ -17,6 +19,7 @@ else
   tar -xvzf ${VERSION}.tar.gz
   cd ${VERSION}
   cargo build --release
+  chmod +x target/release/mc
   mv target/release/mc /usr/local/bin/mc
   cd ..
   rm -rf ${VERSION}
