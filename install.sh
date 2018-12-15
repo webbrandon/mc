a5b126d0fb3ad9d175b3e04bf34452ffa576a8ed
Brandon Clark <brandon.clark@leafgroup.com>
2018-12-14 23:09:18 -0800

    update

--- install.sh
+++ install.sh
@@ -1,7 +1,7 @@
 #!/bin/bash 
 VERSION=v0.2.1
 
-if [[ "$OSTYPE" =~ ^linux ]]; then
+if [[ "$OSTYPE" == "linux-gnu" ]]; then
   echo "Downloading debian client."
   curl -LO https://github.com/webbrandon/mc/releases/download/${VERSION}/debian
   echo "Installing Master of Ceremony ${VERSION}"
@@ -13,19 +13,25 @@ elif [[ "$OSTYPE" == "cygwin" ]]; then
   echo "Installing Master of Ceremony ${VERSION}"
   chmod +x debian
   mv debian /usr/local/bin/mc
+elif [[ "$OSTYPE" == "debian"* ]]; then
+  echo "Downloading debian client."
+  curl -LO https://github.com/webbrandon/mc/releases/download/${VERSION}/debian
+  echo "Installing Master of Ceremony ${VERSION}"
+  chmod +x debian
+  mv debian /usr/local/bin/mc
 elif [[ "$OSTYPE" == "msys" ]]; then
   echo "Downloading debian client."
   curl -LO https://github.com/webbrandon/mc/releases/download/${VERSION}/debian
   echo "Installing Master of Ceremony ${VERSION}"
   chmod +x debian
   mv debian /usr/local/bin/mc
-elif [[ "$OSTYPE" =~ ^freebsd ]]; then
+elif [[ "$OSTYPE" == "freebsd"* ]]; then
   echo "Downloading debian client."
   curl -LO https://github.com/webbrandon/mc/releases/download/${VERSION}/debian
   echo "Installing Master of Ceremony ${VERSION}"
   chmod +x debian
   mv debian /usr/local/bin/mc
-elif [[ "$OSTYPE" =~ ^darwin ]]; then
+elif [[ "$OSTYPE" == "darwin"* ]]; then
   echo "Downloading darwin client."
   curl -LO https://github.com/webbrandon/mc/releases/download/${VERSION}/darwin
   echo "Installing Master of Ceremony ${VERSION}"
@@ -35,12 +41,12 @@ else
   printf "System not supported. Attempting to build from source. You must have rust installed."
   curl -LO https://github.com/webbrandon/mc/archive/${VERSION}.tar.gz
   tar -xvzf ${VERSION}.tar.gz
-  cd mc-${VERSION}
+  cd ${VERSION}
   cargo build --release
   chmod +x target/release/mc
   mv target/release/mc /usr/local/bin/mc
   cd ..
-  rm -rf ${VERSION}
+  rm -rf mc-${VERSION}
 fi
 
 exit 0
