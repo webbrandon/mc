e45c9d15e82ff8fc7d2bbebf2e9552c794e0b24b
Brandon Clark <brandon.clark@leafgroup.com>
2018-12-18 01:51:47 -0800

    init new options

--- src/main.rs
+++ src/main.rs
@@ -4,6 +4,8 @@ extern crate handlebars;
 extern crate serde_json;
 extern crate run_script;
 
+use std::io;
+
 mod cli;
 mod model;
 mod handler;
@@ -18,6 +20,28 @@ fn log_it(mute: bool, content: String) {
     }
 }
 
+fn continue_prompt(step_name: String, ask: bool) {
+    if ask {
+        println!("Would you like to continue with the {} step? (y/N)", step_name);
+        let buffer = &mut String::new();
+        io::stdin().read_line(buffer);
+        match buffer.trim_right() {
+            "true" => true,
+            "TRUE" => true,
+            "True" => true,
+            "T" => true,
+            "t" => true,
+            "false" => false,
+            "FALSE" => false,
+            "False" => false,
+            "F" => false,
+            "f" => false,
+            "" => false,
+            _ => false
+        };
+    }
+}
+
 fn main() {
     let matches = cli::build_cli().get_matches();
     let mut request = Configs::process_args(&matches);
@@ -25,37 +49,48 @@ fn main() {
     let mute = matches.is_present("mute");
     scripts.load_scripts(&mut request); 
     
-    if request.has_build_script() {
-        log_it(mute, scripts.process_build_script().to_owned());
+    if !request.has_no_build() {
+        if request.has_build_script() {
+            log_it(mute, scripts.process_build_script().to_owned());
+        }
     }
     
-    if request.has_template() && request.has_params() {
-        let mut templates = Template::new();
-        templates.load_templates(&mut request);
-        templates.render_template();
-        if request.has_param_script() {
+    if !request.has_no_template() {
+        continue_prompt("template".to_string(), request.has_no_prompt());
+        if request.has_template() && request.has_params() {
+            let mut templates = Template::new();
+            templates.load_templates(&mut request);
+            templates.render_template();
+            if request.has_param_script() {
+                // Need to dig deep I did something that took ownership so I reinitialize with Configs.
+                scripts = Scripts::new();
+                scripts.load_scripts(&mut request); 
+                log_it(mute, scripts.process_param_script().to_owned());
+            }
+            if request.has_render() {
+                file::outfile(request.render(), templates.render());
+                log_it(mute, templates.render().to_string());
+            }
+        }
+    }
+    
+    if !request.has_no_deploy() {
+        continue_prompt("deploy script".to_string(), request.has_no_prompt());
+        if request.has_deploy_script() {
             // Need to dig deep I did something that took ownership so I reinitialize with Configs.
             scripts = Scripts::new();
             scripts.load_scripts(&mut request); 
-            log_it(mute, scripts.process_param_script().to_owned());
-        }
-        if request.has_render() {
-            file::outfile(request.render(), templates.render());
-            log_it(mute, templates.render().to_string());
+            log_it(mute, scripts.process_deploy_script().to_owned());
         }
     }
     
-    if request.has_deploy_script() {
-        // Need to dig deep I did something that took ownership so I reinitialize with Configs.
-        scripts = Scripts::new();
-        scripts.load_scripts(&mut request); 
-        log_it(mute, scripts.process_deploy_script().to_owned());
-    }
-    
-    if request.has_post_script() {
-        // Need to dig deep I did something that took ownership so I reinitialize with Configs.
-        scripts = Scripts::new();
-        scripts.load_scripts(&mut request); 
-        log_it(mute, scripts.process_post_script());        
+    if !request.has_no_post() {
+        continue_prompt("post script".to_string(), request.has_no_prompt());
+        if request.has_post_script() {
+            // Need to dig deep I did something that took ownership so I reinitialize with Configs.
+            scripts = Scripts::new();
+            scripts.load_scripts(&mut request); 
+            log_it(mute, scripts.process_post_script());        
+        }
     }
 }
